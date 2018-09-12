640cd958b346ac3f939ffe84dcea35bbfa773355
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-09-12 17:32:16 +0200

    Started port of genetic.h.

---
+++ src/gq/emiliodallatorre/neuralnet/Genetic.java
@@ -0,0 +1,18 @@
+package gq.emiliodallatorre.neuralnet;
+
+import gq.emiliodallatorre.neuralnet.cppimplementations.Pair;
+
+import java.util.Vector;
+
+class Genetic {
+    SDLnet sdlnet;
+    Vector<Double> graph;
+    float iteration = 1;
+    float time_warp = 1;
+    float sleep=0;
+    Vector<Pair<Float, Pair<Simulation, NeuralNet>>> objects;
+
+    void itinerate(int i) {
+
+    }
+}
