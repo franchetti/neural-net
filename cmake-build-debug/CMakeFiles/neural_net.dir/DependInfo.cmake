295e476561a0ca9796aea1074b5d231a8f305959
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-12-26 12:40:43 +0100

    Created idea project.

---
+++ cmake-build-debug/CMakeFiles/neural_net.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/emili/Programmazione/GitHub/neural-net/main.cpp" "C:/Users/emili/Programmazione/GitHub/neural-net/cmake-build-debug/CMakeFiles/neural_net.dir/main.cpp.obj"
+  "C:/Users/emili/Programmazione/GitHub/neural-net/pch.cpp" "C:/Users/emili/Programmazione/GitHub/neural-net/cmake-build-debug/CMakeFiles/neural_net.dir/pch.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "C:/Users/emili/Programmazione/Librerie/SDL2/include"
+  "C:/Users/emili/Programmazione/Librerie/SDL2_image/include"
+  "C:/Users/emili/Programmazione/Librerie/SDL2_ttf/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
